a3cb9f39548e983bcd69def0066094b2d2aaa01a
simkaiwen <kaiphonegt@gmail.com>
2022-02-20 22:07:47 +0800

    Created derived state managers
    Created AmongUs stateManager derived from Player stateManager

---
+++ Assets/GameAssets/Scripts/AmongUs/AmongUs_StateManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AmongUs))]
+public class AmongUs_StateManager : Player_StateManager<AmongUs> 
+{
+    public List<AmongUsStates> states;
+
+    protected override List<PlayerStates<AmongUs>> GetStateList()
+    {
+        return new List<PlayerStates<AmongUs>>(states);
+    }
+
+}
