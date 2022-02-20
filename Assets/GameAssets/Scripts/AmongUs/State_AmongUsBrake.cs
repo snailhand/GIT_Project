3126af1beec8494bbbedc4ce63ef37c4f10fef92
simkaiwen <kaiphonegt@gmail.com>
2022-02-20 22:19:37 +0800

    Added Brake state for AmongUs

---
+++ Assets/GameAssets/Scripts/AmongUs/State_AmongUsBrake.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class State_AmongUsBrake : AmongUsStates
+{
+	protected override void OnEnter(AmongUs player) { }
+
+	protected override void OnExit(AmongUs player) { }
+
+	protected override void OnStep(AmongUs player)
+	{
+		player.Gravity();
+		player.SnapToGround();
+		player.Jump();
+		player.Fall();
+
+		if (player.horizontalVelocity.sqrMagnitude == 0)
+		{
+			player.states.Change<State_AmongUsIdle>();
+		}
+	}
+
+}
