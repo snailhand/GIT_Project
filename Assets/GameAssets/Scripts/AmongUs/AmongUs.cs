370ea7735da970b3092f8ac4b69135f5cd455c2e
simkaiwen <kaiphonegt@gmail.com>
2022-02-20 22:34:41 +0800

    Fixed AmongUs script derived class ref

--- Assets/GameAssets/Scripts/AmongUs/AmongUs.cs
+++ Assets/GameAssets/Scripts/AmongUs/AmongUs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class AmongUs : Player<AmongUs>
+public class AmongUs : AmongUs<AmongUs>
 {
     public UnityEvent OnJump;
 
