30cab306d4b7815c04ab1af97f5ccf527272631f
simkaiwen <kaiphonegt@gmail.com>
2022-02-21 01:58:17 +0800

    Created Player Health script

---
+++ Assets/GameAssets/Scripts/Health.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public int startHp;
+    public int maxHp;
+    public float cooldown = 1f;
+    public UnityEvent OnChange;
+
+    private float timeSinceLastDamage;
+    private int current;
+
+    private void Awake()
+    {
+        ResetHp();
+    }
+
+    public int currentHp
+    {
+        get { return current; }
+
+        private set
+        {
+            var last = current;
+            
+            if(value != last)
+            {
+                current = value;
+                OnChange?.Invoke();
+            }
+        }
+    }
+
+    public bool isEmpty
+    {
+        get { return current == 0; }
+    }
+
+    public virtual bool recovering
+    {
+        get { return Time.time < timeSinceLastDamage + cooldown; }
+    }
+
+    public virtual void Set(int amount)
+    {
+        currentHp = Mathf.Clamp(currentHp, 0, maxHp);
+    }
+
+    public virtual void Increase(int amount)
+    {
+        currentHp = Mathf.Clamp(currentHp + amount, 0, maxHp);
+    }
+
+    public virtual void Damage(int amount)
+    {
+        if(!recovering)
+        {
+            currentHp -= amount;
+            currentHp = Mathf.Max(current, 0);
+            timeSinceLastDamage = Time.time;
+        }
+    }
+
+    public virtual void ResetHp()
+    {
+        currentHp = startHp;
+    }
+}
