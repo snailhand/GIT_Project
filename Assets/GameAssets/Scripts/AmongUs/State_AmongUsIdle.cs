5a62448dbf560cfb42ab7c87cc5b6bfa4a9d281c
simkaiwen <kaiphonegt@gmail.com>
2022-02-20 22:23:24 +0800

    Added Idle state for AmongUs player

---
+++ Assets/GameAssets/Scripts/AmongUs/State_AmongUsIdle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_AmongUsIdle : AmongUsStates
+{
+	protected override void OnEnter(AmongUs player) { }
+
+	protected override void OnExit(AmongUs player) { }
+
+	protected override void OnStep(AmongUs player)
+	{
+		player.Gravity();
+		player.SnapToGround();
+		player.Jump();
+		player.Fall();
+
+		player.inputs.GetFacingDirection(out var magnitude);
+
+		if (magnitude > 0 || player.horizontalVelocity.sqrMagnitude > 0)
+		{
+			player.states.Change<State_AmongUsWalk>();
+		}
+	}
+}
