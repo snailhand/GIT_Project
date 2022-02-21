711ce167c717b870207316dfd1d3401fd6cfd0d6
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-02-21 18:06:52 +0800

    Implemented Checkpoint, Modified Player Character

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    private GameMasterScript gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMasterScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastCheckPointPos = transform.position;
+        }
+    }
+}
