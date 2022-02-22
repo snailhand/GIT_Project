0b41be46f7aaf1e0a63fc5b7e7e76bd3349e0db5
simkaiwen <kaiphonegt@gmail.com>
2022-02-22 16:59:51 +0800

    Added Hurt state for AmongUs

---
+++ Assets/GameAssets/Scripts/AmongUs/States/State_AmongUsHurt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_AmongUsHurt : AmongUsStates
+{
+	protected override void OnEnter(AmongUs player) { }
+
+	protected override void OnExit(AmongUs player) { }
+
+	protected override void OnStep(AmongUs player)
+	{
+		player.Gravity();
+
+		Debug.Log("State: Hurt");
+
+		if (player.isGrounded && (player.verticalVelocity.y <= 0))
+		{
+			if (player.health.currentHp > 0)
+			{
+				player.states.Change<State_AmongUsIdle>();
+			}
+			else
+			{
+				player.states.Change<State_AmongUsDeath>();
+			}
+		}
+	}
+}
