488437dc5784c82a35e5391abc725a9dee1d812a
simkaiwen <kaiphonegt@gmail.com>
2022-02-22 16:12:55 +0800

    Created Obstacle script

---
+++ Assets/GameAssets/Scripts/Misc/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage;
+
+    private Collider obstacleCollider;
+
+    private void Awake()
+    {
+        obstacleCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(other.TryGetComponent<AmongUs>(out var player))
+            {
+                player.TakeDamage(damage);
+            }
+        }
+    }
+}
