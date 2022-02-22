31971d3491ccd291a77a1d1e95567fa372038022
simkaiwen <kaiphonegt@gmail.com>
2022-02-22 16:59:59 +0800

    Added Death state for AmongUs

---
+++ Assets/GameAssets/Scripts/AmongUs/States/State_AmongUsDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_AmongUsDeath : AmongUsStates
+{
+	protected override void OnEnter(AmongUs player) { }
+
+	protected override void OnExit(AmongUs player) { }
+
+	protected override void OnStep(AmongUs player)
+	{
+		player.Gravity();
+		player.Friction();
+		player.SnapToGround();
+
+		Debug.Log("State: Dead");
+	}
+}
+
