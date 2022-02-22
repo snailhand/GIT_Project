74dffdc6a4b28c70b53f378cb29d8d30dde5888e
simkaiwen <kaiphonegt@gmail.com>
2022-02-22 18:50:00 +0800

    Added Swing Script

---
+++ Assets/GameAssets/Scripts/Misc/Swing.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float currentSpeed;
+    public float maxSpeed;
+    public float minSpeed;
+
+    public float leftAngle;
+    public float rightAngle;
+
+    bool movingClockwise;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movingClockwise = true;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void ChangeMoveDir()
+    {
+        if (movingClockwise && transform.rotation.x > rightAngle)
+        {
+            movingClockwise = false;
+
+        }
+        if (!movingClockwise && transform.rotation.x < leftAngle)
+        {
+            movingClockwise = true;
+
+        }
+
+    }
+
+    private void Move()
+    {
+        ChangeMoveDir();
+
+        if(currentSpeed == maxSpeed)
+        {
+            if (movingClockwise)
+            {
+                rb.angularVelocity = Vector3.right * currentSpeed;
+            }
+
+            if (!movingClockwise)
+            {
+                rb.angularVelocity = Vector3.left * currentSpeed;
+            }
+
+        }
+    }
+}
