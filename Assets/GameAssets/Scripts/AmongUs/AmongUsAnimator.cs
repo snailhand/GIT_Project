9038db2c4829b5b9d63ceb6de0f38f5b6b39b9f1
simkaiwen <kaiphonegt@gmail.com>
2022-02-26 02:13:20 +0800

    Added animation controller & AmongUsAnimator script

---
+++ Assets/GameAssets/Scripts/AmongUs/AmongUsAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AmongUsAnimator : MonoBehaviour
+{
+    public Animator animator;
+    private AmongUs player;
+
+    void Start()
+    {
+        player = GetComponent<AmongUs>();
+    }
+
+    private void LateUpdate()
+    {
+        var state = player.states.index;
+        var horizontalSpeed = player.horizontalVelocity.magnitude;
+        var verticalSpeed = player.verticalVelocity.y;
+
+        animator.SetInteger("State", state);
+        animator.SetFloat("Horizontal Speed", horizontalSpeed);
+        animator.SetFloat("Vertical Speed", verticalSpeed);
+    }
+}
