53640ddfd9bd51e14a93d2ffcbaeb4ab77a8add9
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-02-26 17:31:01 +0800

    Created Scripts for a possible static ranged enemy

---
+++ Assets/GameAssets/Scripts/GunScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject shotPrefab;
+    public Transform[] gunPoints;
+    public float fireRate;
+
+    bool firing;
+    float fireTimer;
+
+    int gunPointIndex;
+
+    void Update()
+    {
+        if (firing)
+        {
+            while (fireTimer >= 1 / fireRate)
+            {
+                SpawnShot();
+                fireTimer -= 1 / fireRate;
+            }
+
+            fireTimer += Time.deltaTime;
+            firing = false;
+        }
+        else
+        {
+            if (fireTimer < 1 / fireRate)
+            {
+                fireTimer += Time.deltaTime;
+            }
+            else
+            {
+                fireTimer = 1 / fireRate;
+            }
+        }
+    }
+
+    void SpawnShot()
+    {
+        var gunPoint = gunPoints[gunPointIndex++];
+        Instantiate(shotPrefab, gunPoint.position, gunPoint.rotation);
+        gunPointIndex %= gunPoints.Length;
+    }
+
+    public void Fire()
+    {
+        firing = true;
+    }
+}
