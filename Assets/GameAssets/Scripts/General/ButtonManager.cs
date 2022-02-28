61dbbfffc0eb80a0f36762bd5f5f1f84e3a51faa
Dygenlow <72850183+Dygenlow@users.noreply.github.com>
2022-02-28 23:35:20 +0800

    Simple Start Button to Level 1

---
+++ Assets/GameAssets/Scripts/General/ButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void Instruction()
+    {
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
