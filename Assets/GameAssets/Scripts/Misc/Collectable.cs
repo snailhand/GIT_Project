64d9caf0f35ac17103bc1f414a17493f0ca031f7
simkaiwen <kaiphonegt@gmail.com>
2022-02-28 23:07:42 +0800

    Created Collectable script

---
+++ Assets/GameAssets/Scripts/Misc/Collectable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+	public GameObject display;
+	public AudioClip clip;
+
+	private Collider _collider;
+
+	public virtual void Collect(AmongUs player)
+	{
+		display.SetActive(false);
+		_collider.enabled = false;
+	}
+
+	private void Awake()
+	{
+		_collider = GetComponent<Collider>();
+		_collider.isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			if (other.TryGetComponent<AmongUs>(out var player))
+			{
+				Collect(player);
+			}
+		}
+	}
+}
+}
